562972cc2e4fa9e58c743aeb0c414fbefa451689
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-04-25 10:01:47 +0200

    new function for drat upload

--- R/zzz.R
+++ R/zzz.R
@@ -16,12 +16,13 @@
 
 #' Provide some basic information about the code of the package
 #'
-#' This function provides the number of functions and lines of code of the
-#' package. The results depend on whether the function is called from the
-#' compiled or the raw package. When called from the compiled package, the
-#' number of functions only gives the number of exported functions, and the
-#' number of lines of code no longer includes documentation and examples. This
-#' function is for development purpose only and is thus not exported.
+#' This is a function for development purpose only. This function provides the
+#' number of functions and lines of code of the package. The results depend on
+#' whether the function is called from the compiled or the raw package. When
+#' called from the compiled package, the number of functions only gives the
+#' number of exported functions, and the number of lines of code no longer
+#' includes documentation and examples. This function is for development purpose
+#' only and is thus not exported.
 #'
 info_package <- function() {
   print(paste("number of functions =", length(ls("package:SileR"))))
@@ -32,4 +33,26 @@ info_package <- function() {
     print(paste("number of lines of code =", lines_code))
   } else {message("Install the package R.utils for more info.")}
   return(invisible(NULL))
+}
+
+#' Build and upload the package on drat
+#'
+#' This is a function for development purpose only. Drat is a package that
+#' allows for the installation of R packages from GitHub. We use this system to
+#' distribute the package (see Readme.Rmd). The function defined here builds the
+#' package, commits the changes and push them on the drat repository on GitHub.
+#' It is designed to work on the maintainer computer only.
+#'
+upload_on_drat <- function(){
+  if (requireNamespace("devtools") & requireNamespace("drat")) {
+    print("Building the package...")
+    path <- devtools::build(binary = FALSE)
+    print("Moving the package to local drat folder and commiting changes in git...")
+    drat::insertPackage(path, "~/Boulot/Mes_projets_de_recherche/R_packages/drat", commit = TRUE)
+    print("Pushing the package on GitHub...")
+    system("cd ~/Boulot/Mes_projets_de_recherche/R_packages/drat; git push")
+    print("done")
+  } else {
+    stop("devtools and drat must both be installed for this function to work.")
+  }
 }
